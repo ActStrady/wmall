ae77e68eafd9111ce5f77d35c2ae517ded6d37a4
王强 <ActStrady@tom.com>
2019-10-21 14:50:36 +0800

    spring data rest 搭建成功

--- wmall-model/src/main/java/com/actstrady/wmall/po/User.java
+++ wmall-model/src/main/java/com/actstrady/wmall/po/User.java
@@ -1,5 +1,6 @@
 package com.actstrady.wmall.po;
 
+import lombok.Cleanup;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +8,9 @@ import javax.persistence.*;
 import java.util.Date;
 
 /**
- * Entity
+ * Entity 表示这是一个需要orm的实体类,name表示映射表名
+ * Table 用来设置表的属性，很多属性，设置name后会优先于entity设置
+ *
  * @author : ActStrady@tom.com
  * @date : 2019/10/11 20:03
  * @fileName : UserPO.java
@@ -15,18 +18,23 @@ import java.util.Date;
  */
 @Data
 @RequiredArgsConstructor
-@Entity(name = "user")
+@Entity
 public class User {
+    /**
+     * mysql 使用GenerationType.IDENTITY规则主键自增
+     */
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String username;
     private String password;
     private String name;
     private String email;
-    @Column(name = "is_delete")
+    @Column(name = "is_delete", insertable = false)
     private Boolean delete;
+    @Column(insertable = false)
     private Date createTime;
+    @Column(insertable = false)
     private Date updateTime;
     private Date deleteTime;
 }
